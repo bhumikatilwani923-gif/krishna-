cc2b776e86c6532dc795a80e0f35071339d3880c
bhumikatilwani923-gif <bhumikatilwani923@gmail.com>
2025-12-26 14:24:30 +0530

    Add Button.js

---
+++ components/ui/Button.js
@@ -0,0 +1,7 @@
+export function Button({ children, className, ...props }) {
+  return (
+    <button className={className + " px-4 py-2 rounded"} {...props}>
+      {children}
+    </button>
+  );
+}
